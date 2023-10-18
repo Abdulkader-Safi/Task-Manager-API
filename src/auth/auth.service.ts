19967e514c6718d0042606048ff9697b5e6e48e0
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-18 12:57:05 +0300

    hash signup password and built basic signin api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { log } from 'console';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Users } from './users.entity';
 import { UsersRepository } from './users.repository';
@@ -13,7 +20,35 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.usersRepository.create({ username, password });
-    await this.usersRepository.save(user);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      log(error.code);
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'password';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
--- src/auth/users.entity.ts
+++ src/auth/users.entity.ts
@@ -11,7 +11,7 @@ export class Users {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
   @Column()
