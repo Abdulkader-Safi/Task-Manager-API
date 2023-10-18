9d00b30efc3f245d21250ce673593d66f112cda2
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-18 12:21:46 +0300

    create basic signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Users } from './users.entity';
 import { UsersRepository } from './users.repository';
 
@@ -8,4 +9,11 @@ export class AuthService {
   constructor(
     @InjectRepository(Users) private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.usersRepository.create({ username, password });
+    await this.usersRepository.save(user);
+  }
 }
